60f683c0d762951ffa59856b3002525d00ae1d77
AnawinZ <ext.anawin.khl@truedigital.com>
2022-06-26 23:19:22 +0700

    add localStrorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { LocalizedString } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { json } from 'express';
 import { Todo } from './Todo';
 
 @Component({
@@ -9,7 +11,7 @@ import { Todo } from './Todo';
 export class AppComponent {
   todos : Todo[] = [];
   newTodo : string;
-  
+
   saveTodo(){
     if(this.newTodo){
       let todo = new Todo();
@@ -20,17 +22,28 @@ export class AppComponent {
     }else{
       alert('Please Add Tasks');
     }
+    localStorage.setItem('list',JSON.stringify(this.todos));
   } 
+
   done(id:number){
     this.todos[id].IsCompleted =! this.todos[id].IsCompleted;
   }
+
   remove(id:number){
     this.todos = this.todos.filter((todo,number)=> number !== id);
+
+    localStorage.setItem('list',JSON.stringify(this.todos));
   }
+
   clearCompleted(): void {
     this.todos = this.todos.filter((todo,Number) => todo.IsCompleted);
+
+    localStorage.setItem('list',JSON.stringify(this.todos));
   }
+
   clearAll(): void {
     this.todos = this.todos.filter((todo) => !this.done);
+
+    localStorage.clear();
   }
 }
